633b25e4633e93219218758b1ddf8e6f3bb9c868
kiboma <omayioben@gmail.com>
2024-03-11 18:44:21 +0300

    Update projects

--- data/projects.js
+++ data/projects.js
@@ -35,32 +35,62 @@ const projects = [
     source: "https://github.com/kiboma2021/furahia-insurance?tab=readme-ov-file",
   },
 
+  // {
+  //   title: "SHUTTERSWAP CRM",
+  //   description: "Shutterswap is a Django-based application designed to streamline customer management, item tracking, and financial recording. It offers functionalities to maintain customer records, monitor lent items, receive returned items, and track both income and expenses efficiently.",
+  //   imageUrl: "./images/project_images/SHUTTERSWAP.png",
+  //   technologies: ["Django", "CSS", "HTML","JavaScript"],
+  //   liveVersion: "/",
+  //   source: "https://github.com/kiboma2021/CRM",
+  // },
+
+  // {
+  //   title: "QUERY MANAGEMENT SYSTEM (QMS)",
+  //   description: "This Django web-based application facilitates the tracking of queries received via emails. It provides tools to monitor and manage the turnaround time from ticket creation to resolution, ensuring efficient handling of inquiries.",
+  //   imageUrl: "./images/project_images/QMS.png",
+  //   technologies: ["Django", "CSS", "HTML","JavaScript"],
+  //   liveVersion: "https://www.google.com/",
+  //   source: "https://github.com/kiboma2021/APA-CRM",
+  // },
+
+  // {
+  //   title: "PENSION MANAGEMENT SYSTEM",
+  //   description: "This Django web-based application facilitates the tracking of queries received via emails. It provides tools to monitor and manage the turnaround time from ticket creation to resolution, ensuring efficient handling of inquiries.",
+  //   imageUrl: "./images/project_images/PENSION.png",
+  //   technologies: ["Django", "CSS", "HTML","JavaScript"],
+  //   liveVersion: "https://www.google.com/",
+  //   source: "https://github.com/kiboma2021/APA-CRM",
+  // },
+
+
+
   {
-    title: "SHUTTERSWAP CRM",
-    description: "Shutterswap is a Django-based application designed to streamline customer management, item tracking, and financial recording. It offers functionalities to maintain customer records, monitor lent items, receive returned items, and track both income and expenses efficiently.",
-    imageUrl: "./images/project_images/SHUTTERSWAP.png",
-    technologies: ["Django", "CSS", "HTML","JavaScript"],
-    liveVersion: "/",
-    source: "https://github.com/kiboma2021/CRM",
+    title: "REDUX SHOPMATE",
+    description: "This website focuses on managing items in the shopping cart, utilizing React and Redux. Users can effortlessly add or remove items from their cart with ease.",
+    imageUrl: "./images/project_images/REDUX_SHOPMATE.png",
+    technologies: ["React","Redux", "Tailwind"],
+    liveVersion: "https://redux-shopmate.netlify.app/",
+    source: "https://github.com/kiboma2021/Redux-Shopmate",
   },
 
   {
-    title: "QUERY MANAGEMENT SYSTEM (QMS)",
-    description: "This Django web-based application facilitates the tracking of queries received via emails. It provides tools to monitor and manage the turnaround time from ticket creation to resolution, ensuring efficient handling of inquiries.",
-    imageUrl: "./images/project_images/QMS.png",
-    technologies: ["Django", "CSS", "HTML","JavaScript"],
-    liveVersion: "https://www.google.com/",
-    source: "https://github.com/kiboma2021/APA-CRM",
+    title: "FOOD DELIVERY APP",
+    description: "This website focuses on managing items within the shopping cart of a food delivery store. Developed using React and Redux, users can seamlessly add or remove items from their cart.",
+    imageUrl: "./images/project_images/FOOD_DELIVERY.png",
+    technologies: ["React","Redux", "Tailwind"],
+    liveVersion: "https://orderfoodke.netlify.app/",
+    source: "https://github.com/kiboma2021/React-Food-Delivery-App",
   },
 
   {
-    title: "PENSION MANAGEMENT SYSTEM",
-    description: "This Django web-based application facilitates the tracking of queries received via emails. It provides tools to monitor and manage the turnaround time from ticket creation to resolution, ensuring efficient handling of inquiries.",
-    imageUrl: "./images/project_images/PENSION.png",
-    technologies: ["Django", "CSS", "HTML","JavaScript"],
-    liveVersion: "https://www.google.com/",
-    source: "https://github.com/kiboma2021/APA-CRM",
+    title: "TASKMATE",
+    description: "The app demonstrates state, useState, Components,Props, Global Level Styling, Component Level Styling, Inline Level Styling and Module Level Styling, User Inputs, useEffect and localStorage",
+    imageUrl: "./images/project_images/TASKMATE.png",
+    technologies: ["React","Redux", "Tailwind"],
+    liveVersion: "https://taskmate-kibs.netlify.app/",
+    source: "https://github.com/kiboma2021/React-TaskMate",
   },
+
 
 ];
 
